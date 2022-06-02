2c282d44f2a11cfce36e0663f8467c3b0813e30c
PrzemyslawWozniakowski <przemyslawwozniakowski@gmail.com>
2022-06-02 18:37:25 +0200

    Update node.py

--- node.py
+++ node.py
@@ -35,7 +35,7 @@ class Node:
         return wins - loses
 
     '''
-    Returns an amount 
+    Returns an amount of times the node was visited
     '''
     def n(self):
         return self._number_of_visits
@@ -71,7 +71,7 @@ class Node:
         return current_rollout_state.game_result()
 
     '''
-    Backpropagate through visited nodes and updates statistics
+    Backpropagates through visited nodes and updates statistics
     '''
     def backpropagate(self, result):
         self._number_of_visits += 1.
